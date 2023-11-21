590654b4f13494af40af187daf061035e963b81c
Antonio <antoniolm386@gmail.com>
2023-11-21 08:00:42 -0600

    feat(labels)

--- src/gui/swing/frames/MyfirstFrame.java
+++ src/gui/swing/frames/MyfirstFrame.java
@@ -1,6 +1,7 @@
 package gui.swing.frames;
 
 import java.awt.Color;
+import java.net.URL;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -10,7 +11,9 @@ public class MyfirstFrame extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	
+	URL urlIcon = MyfirstFrame.class.getResource("icon.png");
+	
 	public MyfirstFrame() {
 		this.setTitle("My first Java Frame");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of application
@@ -18,7 +21,7 @@ public class MyfirstFrame extends JFrame {
 		this.setSize(400, 400); //sets the frame's dimensions
 		this.setVisible(true);  //make frame visible
 		
-		ImageIcon img = new ImageIcon("../icon.png"); //create an ImageIcon
+		ImageIcon img = new ImageIcon(urlIcon); //create an ImageIcon
 		this.setIconImage(img.getImage()); // set the icon
 		
 		this.getContentPane().setBackground(new Color(0x54787d)); //change color bg, we can use rgb, hex...
